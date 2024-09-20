1f7855c83e477633c65e34350963916338b662bf
Santehnik01 <125136026+Santehnik01@users.noreply.github.com>
2024-09-20 11:24:17 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,30 @@
-t = (i for i in range(5))
+from selenium import webdriver  
+from selenium.webdriver.common.by import By  
+from selenium.webdriver.support.ui import WebDriverWait  
+from selenium.webdriver.support import expected_conditions as EC  
+
+
+driver = webdriver.Chrome()  
+driver.implicitly_wait(10)  
+
+try:  
+    # Открываем страницу  
+    driver.get('')  
+
+    
+    element = driver.find_element(By.ID, '')  
+    print(element.text)  # Работа с элементом  
+
+      
+    wait = WebDriverWait(driver, 10)   
+    button = wait.until(EC.element_to_be_clickable((By.ID, '')))  
+    button.click()  
+
+    # Пример ожидания, пока элемент не станет видимым  
+    # wait.until(EC.visibility_of_element_located((By.CLASS_NAME, '')))  
+
+finally:  
+    driver.quit()  
 
 
     
